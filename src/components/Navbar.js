715ac42ad100e5983730e16e3ca4581406ed986f
Roman <alonzofroman@gmail.com>
2022-05-14 12:16:12 -0700

    Added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h1>GameShock</h1>
+            <a href="/#">Employee Login</a>
+
+        </div>
+        )
+}
+
