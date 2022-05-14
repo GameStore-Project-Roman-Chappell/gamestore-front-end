5b09920a7d1befb7c00734da6820d37a1829524d
Roman <alonzofroman@gmail.com>
2022-05-14 12:53:24 -0700

    Added route for restock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar";
 import Restock from "./components/Restock";
+import {BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Restock />
+      <Router>
+        
+        <Route exact path='/employee' component={Restock}/>
+      </Router>
+      
     </div>
   );
 }
