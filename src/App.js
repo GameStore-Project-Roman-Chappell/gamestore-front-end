18359c65c7ced3164b3930e6b0608a99dde17047
Roman <alonzofroman@gmail.com>
2022-05-16 12:05:35 -0700

    Added routes for order forms to app page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./gamestore.css";
 import Navbar from "./components/Navbar";
 import Shop from "./components/Shop";
 import Restock from "./components/Restock";
+import GameOrder from "./components/GameOrder";
+import ConsoleOrder from "./components/ConsoleOrder";
+import ShirtOrder from "./components/ShirtOrder";
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +15,10 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Shop />}/>
         <Route exact path='/employee' element={<Restock />}/>
+        <Route exact path='/gameOrder' element={<GameOrder />}/>
+        <Route exact path='/consoleOrder' element={<ConsoleOrder />}/>
+        <Route exact path='/shirtOrder' element={<ShirtOrder />}/>
+
       </Routes>
       </Router>
     </div>
